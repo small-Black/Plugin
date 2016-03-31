39f10a8c25abf4a99ed8eaf9cb966ab486178fcb
littleBlack520 <649925003@qq.com>
2016-03-31 15:37:37 +0800

    新增操作Class的两个函数

--- Tool/js/Tool.js
+++ Tool/js/Tool.js
@@ -12,3 +12,46 @@
      }
      return argUrl;
  }
+ // 添加Class
+ function addClass(elem, value) {
+     var classNames, i, l,
+         setClass, c, cl,
+         space = /\s+/;
+     if (value && typeof value === "string") {
+         classNames = value.split(space);
+         if (elem.nodeType === 1) {
+             if (!elem.className && classNames.length === 1) {
+                 elem.className = value;
+
+             } else {
+                 setClass = " " + elem.className + " ";
+
+                 for (c = 0, cl = classNames.length; c < cl; c++) {
+                     if (setClass.indexOf(" " + classNames[c] + " ") < 0) {
+                         setClass += classNames[c] + " ";
+                     }
+                 }
+                 elem.className = setClass.replace(/(^\s*)|(\s*$)/g, "");
+             }
+         }
+     }
+ }
+ // 移除Class
+ function removeClass(elem, value) {
+     var removes, className, c, cl, i, l, space = /\s+/,
+         rclass = /[\t\r\n]/g;
+     if ((value && typeof value === "string") || value === undefined) {
+         removes = (value || "").split(space);
+         if (elem.nodeType === 1 && elem.className) {
+
+             className = (" " + elem.className + " ").replace(rclass, " ");
+             for (c = 0, cl = removes.length; c < cl; c++) {
+                 while (className.indexOf(" " + removes[c] + " ") >= 0) {
+                     className = className.replace(" " + removes[c] + " ", " ");
+                 }
+             }
+             elem.className = value ? className.replace(/(^\s*)|(\s*$)/g, "") : "";
+         }
+     }
+
+ }
